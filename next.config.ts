43057f254f181c2dd0d88e3a5bc0952be93a442c
Zhanna Rohalska <zhanna.rohalska@Mac.lan>
2026-09-23 13:36:29 -0400

    Configure fakestoreapi.com image hostname

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
       },
     ],
   },
